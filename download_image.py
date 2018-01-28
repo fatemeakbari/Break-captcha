2b44798edb1a2ee3f1abe507796ee42f5d3703f5
fatemeakbari <fatemeakbari76.oh@gmail.com>
2018-01-28 15:38:21 +0330

    Add files via upload
    final break captcha

---
+++ download_image.py
@@ -0,0 +1,40 @@
+import argparse
+import requests
+import time
+import os
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-o", "--output",
+                default="D:\project_Python\captcha_breaker\downloads",
+                help="path to output directory of images")
+ap.add_argument("-n", "--num-images", type=int,
+default=300, help="# of images to download")
+args = vars(ap.parse_args())
+# initialize the URL that contains the captcha images that we will
+# be downloading along with the total number of images downloaded
+# thus far
+url = "http://edu.sharif.edu/jcaptcha.jpg"
+total = 0
+#We are now ready to download the captcha images:
+# loop over the number of images to download
+for i in range(0, args["num_images"]):
+    try:
+        # try to grab a new captcha image
+        r = requests.get(url, timeout=60)
+        
+        # save the image to disk
+        p = os.path.sep.join([args["output"], "{}.jpg".format(
+        str(total).zfill(5))])
+        f = open(p, "wb")
+        f.write(r.content)
+        f.close()
+        # update the counter
+        print("[INFO] downloaded: {}".format(p))
+        total += 1
+    
+    # handle if any exceptions are thrown during the download process
+    except:
+        print("[INFO] error downloading image...")
+    
+    # insert a small sleep to be courteous to the server
+   
